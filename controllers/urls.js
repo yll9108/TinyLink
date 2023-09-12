41da99ab51a3dd82da4967d0c9b2c38b31328fcb
YenLun <yenlunliu@gmail.com>
2023-09-12 16:46:15 -0700

    adding urls/new done

--- controllers/urls.js
+++ controllers/urls.js
@@ -3,3 +3,10 @@
 export const renderUrls = (req, res) => {
     res.render("urls");
 };
+
+export const editUrls = (req, res) => {
+    res.render("singleUrl");
+};
+
+// conditionally rendering, shouldn't show Login & Register
+// add a button to create newUrl ( route to newUrl)
--- routes/urls.js
+++ routes/urls.js
@@ -1,8 +1,12 @@
 import express from "express";
 const urlsRouter = express.Router();
 
-import { renderUrls } from "../controllers/urls.js";
+import { renderUrls, editUrls } from "../controllers/urls.js";
 
 urlsRouter.get("/", renderUrls);
 // console.log("urlsRouter works");
+
+urlsRouter.get("/new", editUrls);
+console.log("editUrls works");
+
 export default urlsRouter;
