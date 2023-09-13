23c782fcf33261a93284c23d00aa572d30457501
YenLun <yenlunliu@gmail.com>
2023-09-12 17:05:43 -0700

    server is working - revised

--- controllers/urls.js
+++ controllers/urls.js
@@ -4,9 +4,9 @@ export const renderUrls = (req, res) => {
     res.render("urls");
 };
 
-export const editUrls = (req, res) => {
-    res.render("singleUrl");
-};
+// export const singleUrls = (req, res) => {
+//     res.render("singleUrl");
+// };
 
 // conditionally rendering, shouldn't show Login & Register
 // add a button to create newUrl ( route to newUrl)
--- routes/urls.js
+++ routes/urls.js
@@ -1,12 +1,12 @@
 import express from "express";
 const urlsRouter = express.Router();
 
-import { renderUrls, editUrls } from "../controllers/urls.js";
+import { renderUrls } from "../controllers/urls.js";
 
 urlsRouter.get("/", renderUrls);
 // console.log("urlsRouter works");
 
-urlsRouter.get("/new", editUrls);
-console.log("editUrls works");
+// urlsRouter.get("/new", singleUrls);
+// console.log("editUrls works");
 
 export default urlsRouter;
