273a4059575b7f197dbd1105903adc22fd1f82ce
YenLun <yenlunliu@gmail.com>
2023-09-14 15:24:34 -0700

    bcrypt compare password done

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,61 +1,86 @@
 import { getAllUsers } from "../helpers/users.js";
 import { writeDataToFile, reading } from "../utils/updateDB.js";
-import bcrypt from "bcrypt"
-import { v4 as uuidv4 } from 'uuid';
+import bcrypt from "bcrypt";
+import { v4 as uuidv4 } from "uuid";
+import fs from "fs";
+import path from "path";
 
 const dbPath = "./models/users.json";
 
-export const renderRegister = (req, res) => {
+export const renderLogin = (req, res) => {
+    res.render("login");
+};
 
+export const renderRegister = (req, res) => {
     res.render("register");
     // const users = getAllUsers()
     // res.send(users)
-
 };
-
 
 const validate = (email, db) => {
     let found = false;
     db.users.find((user) => {
         if (email === user.email) {
-            found = true
+            found = true;
         }
-    })
-    return found
-}
-
-
-
+    });
+    return found;
+};
 
 export const newUser = (req, res) => {
-
-    if (req.body.name === " " || req.body.email === " " || req.body.password === "") {
-        res.send("Please fill out the fields")
+    if (
+        req.body.name === " " ||
+        req.body.email === " " ||
+        req.body.password === ""
+    ) {
+        res.send("Please fill out the fields");
     }
 
-    console.log("req", req.body)
+    console.log("req", req.body);
 
-    const db = reading(dbPath)
+    const db = reading(dbPath);
     if (validate(req.body.email, db)) {
-        res.send("Error")
+        res.send("Error");
     } else {
         try {
             const saltRounds = 12;
             const salt = bcrypt.genSaltSync(saltRounds);
             const hash = bcrypt.hashSync(req.body.password, salt);
-            let id = uuidv4()
+            let id = uuidv4();
             const newUser = {
                 id: id,
                 name: req.body.name,
                 email: req.body.email,
-                password: hash
-            }
-            writeDataToFile(dbPath, newUser)
-            res.redirect("/")
+                password: hash,
+            };
+            writeDataToFile(dbPath, newUser);
+            res.redirect("/");
         } catch (error) {
-            throw new Error()
+            throw new Error();
         }
     }
-}
+};
 
-
+export const checkACandSetCookie = (req, res) => {
+    const __dirname = path.resolve();
+    const userDataPath = path.join(__dirname, dbPath);
+    const userData = fs.readFileSync(userDataPath, "utf-8");
+    const users = JSON.parse(userData);
+    const { email, password } = req.body;
+    // const userArray = Object.values(users);
+    // console.log("userArray", userArray);
+    const user = users.users.find((user) => user.email === email);
+    console.log("user", user);
+    if (user) {
+        bcrypt.compare(password, user.password, (err, result) => {
+            if (result) {
+                req.session.user = user;
+                res.redirect("/urls");
+            } else {
+                res.status(401).send("Invaild email or password");
+            }
+        });
+    } else {
+        res.status(401).send("sth went wrong");
+    }
+};
--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-import fs from "fs";
 import express from "express";
-import path from "path";
 import cookieSession from "cookie-session";
 import Keygrip from "keygrip";
 const server = express();
 import urlsRouter from "./routes/urls.js";
-import usersRouter from "./routes/auth.js"
+import { usersRouter, loginRouter } from "./routes/auth.js";
 
 server.set("view engine", "ejs");
-server.use(express.json());
-server.use(express.urlencoded({ extended: true }));
-
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
@@ -29,36 +24,8 @@ server.use((req, res, next) => {
     next();
 });
 
-const __dirname = path.resolve();
-
-server.get("/", (req, res) => {
-    res.render("login", { root: __dirname });
-});
-
-const userDataPath = path.join(__dirname, "models/users.json");
-const userData = fs.readFileSync(userDataPath, "utf-8");
-const users = JSON.parse(userData);
-
-server.post("/", (req, res) => {
-    console.log(req.body);
-    // res.send("HEllo");
-
-    const { email, password } = req.body;
-    const userArray = Object.values(users);
-    const user = userArray.find(
-        (user) => user.email === email && user.password === password
-    );
-
-    if (user) {
-        req.session.user = user;
-        console.log("req.session.user", req.session.user);
-        res.redirect("/urls");
-    } else {
-        res.status(401).send("Invaild email or password");
-    }
-});
-
+server.use("/", loginRouter);
 server.use("/urls", urlsRouter);
-server.use("/users", usersRouter)
+server.use("/users", usersRouter);
 
 export default server;
