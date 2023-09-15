ced3024e73899a197dc6573c1953869764fec387
sujin323 <56923051+sujin323@users.noreply.github.com>
2023-09-15 06:08:46 -0700

    update createUrl

--- controllers/urls.js
+++ controllers/urls.js
@@ -1,5 +1,6 @@
 // urls functions
 import shortid  from "shortid";
+import { writeDataToFile} from "../utils/updateDB";
 
 export const renderUrls = (req, res) => {
     res.render("urls");
@@ -17,6 +18,16 @@ export const createUrl = (req, res) => {
     const { longUrl } = req.body;
     const shortUrlId = shortid.generate();
     console.log(`Shortened URL: ${shortUrlId}`);
-
+    
+    // Read existing data from the file
+    const data = reading("models/urls.json");
+    const newUrl = {
+        longUrl,
+        shortUrlId,
+        userID
+    };
+    data.urls.push(newUrl);
+    writeDataToFile("models/urls.json", data);
+    // Redirect to /urls
     res.redirect("/urls");
 };
