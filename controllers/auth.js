97045540fac82d7d7ce4a41041a05a4bc6a332e2
YenLun <yenlunliu@gmail.com>
2023-09-15 12:49:56 -0700

    create and send token to web done

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ import { getAllUsers, validate } from "../helpers/users.js";
 import { writeDataToFile, reading } from "../utils/updateDB.js";
 import bcrypt from "bcrypt";
 import { v4 as uuidv4 } from "uuid";
+import jwt from "jsonwebtoken";
 
 const dbPath = "./models/users.json";
 
@@ -9,61 +10,71 @@ export const renderLogin = (req, res) => {
     res.render("login");
 };
 
-
 export const renderRegister = (req, res) => {
     res.render("register", { error: false, message: "" });
     // const users = getAllUsers()
     // res.send(users)
-
 };
 
-
-
-
-
-
-
-
 export const newUser = (req, res) => {
-    if (req.body.name === " " || req.body.email === " " || req.body.password === "") {
-        res.render("register", { error: true, message: "Please fill out all the fields." })
+    if (
+        req.body.name === " " ||
+        req.body.email === " " ||
+        req.body.password === ""
+    ) {
+        res.render("register", {
+            error: true,
+            message: "Please fill out all the fields.",
+        });
     } else {
-        const db = reading(dbPath)
+        const db = reading(dbPath);
         if (validate(req.body.email, db)) {
-            res.render("register", { error: true, message: "User already registered" })
+            res.render("register", {
+                error: true,
+                message: "User already registered",
+            });
         } else {
-
             const saltRounds = 12;
             const salt = bcrypt.genSaltSync(saltRounds);
             const hash = bcrypt.hashSync(req.body.password, salt);
-            let id = uuidv4()
+            let id = uuidv4();
             const newUser = {
                 id: id,
                 name: req.body.name,
                 email: req.body.email,
-                password: hash
-            }
-            writeDataToFile(dbPath, newUser)
-            res.redirect("/")
-
+                password: hash,
+            };
+            writeDataToFile(dbPath, newUser);
+            res.redirect("/");
         }
     }
-}
+};
+
+const maxAge = 1 * 24 * 60 * 60;
+
+export const createToken = (id) => {
+    return jwt.sign({ id }, "net random secret", {
+        expiresIn: maxAge,
+    });
+    // create a function whcih creates a token (with signature, payload,...)
+};
 
 export const checkACandSetCookie = (req, res) => {
-    // const __dirname = path.resolve();
-    // const userDataPath = path.join(__dirname, dbPath);
     const users = getAllUsers();
     const { email, password } = req.body;
-    // const userArray = Object.values(users);
-    // console.log("userArray", userArray);
     const user = users.users.find((user) => user.email === email);
     console.log("user", user);
     if (user) {
         bcrypt.compare(password, user.password, (err, result) => {
             if (result) {
+                const token = createToken(user.id);
+                res.cookie("jwt", token, {
+                    httpOnly: true,
+                    maxAge: maxAge * 1000,
+                });
                 req.session.user = user;
                 res.redirect("/urls");
+                console.log("req.session.user", req.session.user);
             } else {
                 res.status(401).send("Invaild email or password");
             }
