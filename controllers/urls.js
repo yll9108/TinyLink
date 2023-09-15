b84a10594b1c62e2b1df87177276c431f7eb17ec
Diogo Krub de Almeida <diogokalmeida@gmail.com>
2023-09-15 13:12:00 -0700

    User ID added to URL

--- controllers/urls.js
+++ controllers/urls.js
@@ -1,11 +1,11 @@
 // urls functions
-import shortid  from "shortid";
+import shortid from "shortid";
 import { readingURL, writeDataToFileURL } from "../utils/updateDB.js";
 
 export const renderUrls = (req, res) => {
     const data = readingURL("models/urls.json");
     res.render("urls", { urls: data.urls });
-  };
+};
 
 export const newUrls = (req, res) => {
     res.render("newUrl");
@@ -19,15 +19,17 @@ export const createUrl = (req, res) => {
     const { longUrl } = req.body;
     const shortUrlId = shortid.generate();
     // console.log(`Shortened URL: ${shortUrlId}`);
-    
+
+    let userId = req.session.user.id
+
     const data = readingURL("models/urls.json");
 
     const newUrl = {
         id: shortUrlId,
         longUrl,
-        // userID 
+        userId,
     };
-    
+
     data.urls.push(newUrl);
 
     writeDataToFileURL("models/urls.json", data);
