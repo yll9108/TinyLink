42cbdcce4a89de1f5d8190dada1b512e16d3d67b
sujin323 <56923051+sujin323@users.noreply.github.com>
2023-09-16 04:22:33 -0700

    deleteURL, check user logged in

--- routes/urls.js
+++ routes/urls.js
@@ -1,7 +1,7 @@
 import express from "express";
 const urlsRouter = express.Router();
 
-import { renderUrls, newUrls, singleUrl, createUrl } from "../controllers/urls.js";
+import { renderUrls, newUrls, singleUrl, createUrl, deleteUrl } from "../controllers/urls.js";
 
 urlsRouter.get("/", renderUrls);
 // console.log("urlsRouter works");
@@ -14,5 +14,8 @@ urlsRouter.get("/:id", singleUrl);
 
 urlsRouter.post("/new", createUrl);
 // console.log("createUrl works");
+
+urlsRouter.post("/:id/delete", deleteUrl);
+// console.log("deleteUrl works");
 
 export default urlsRouter;
